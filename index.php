baabcc1a6bc81cc0948229e6588c74d4632d42d1
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-07-13 07:46:55 +0200

    creato oggetto email in index e stampata a schermo

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
     //usiamo il require_once per evitare eventuali errori di ripetizione
     require_once __DIR__.'/Models/SistemiComunicazione.php';
-
-    $comm = new SistemiComunicazione('Fabrizio', 'Antonello', 'Saluto', 'Ciao Antonello');
+    require_once __DIR__.'/Models/Email.php';
+    $comm = new SistemiComunicazione('Giangiorgio', 'Fumagalli', 'Saluto', 'Ciao Fumagalli');
 
     //var_dump($comm);
 
@@ -12,6 +12,26 @@
     echo "Oggetto: ".$comm->getOggetto()."<br/>";
     echo "Contenuto: ".$comm->getContenuto()."<br/>";
     echo "Stato invio: ".$comm->invio()."<br/>";
+
+    $email = new Email('emanuelebrusa@hotmail.it', 'pietropietra@hotmail.it', 'Email test', 'Contenuto Email per Pietro', true);
+
+    echo "<hr>";
+    echo "Mittente: ".$email->getMittente()."<br>";
+    echo "Destinatario: ".$email->getDestinatario()."<br/>";
+    echo "Oggetto: ".$email->getOggetto()."<br/>";
+    echo "Contenuto: ".$email->getContenuto()."<br/>";
+    echo "Notifica Consegna: ".$email->getNotificaConsegna()."<br/>";
+    /*oppure:
+    if($email->getNotificaConsegna()){
+        echo "L'email è stata consegnata correttamente al destinatario";
+    }
+    else{
+        echo "L'email non è stata inviata. Riprova più tardi.";
+    }
+    */
+    echo "Stato invio: ".$email->invio()."<br/>";
+    echo "Inoltro: ".$email->inoltro()."<br/>";
+    echo "Stampa: ".$email->stampa()."<br/>";
 ?>
 
 <!DOCTYPE html>
