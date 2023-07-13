25c73b0e9074de54173ec45f491318386dee7921
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-07-13 07:10:33 +0200

    Creazione metodi, collegamento a index e stampa

--- Models/SistemiComunicazione.php
+++ Models/SistemiComunicazione.php
@@ -12,5 +12,43 @@
             $this->oggetto = $oggetto;
             $this->contenuto = $contenuto;
         }
+
+        //creazione metodi per poter usare le proprietà private (getter e setter)
+        public function setMittente($mittente){
+            $this->mittente = $mittente;
+        }
+
+        public function getMittente(){
+            return $this->mittente;
+        }
+
+        public function setDestinatario($destinatario){
+            $this->destinatario = $destinatario;
+        }
+
+        public function getDestinatario(){
+            return $this->destinatario;
+        }
+
+        public function setOggetto($oggetto){
+            $this->oggetto = $oggetto;
+        }
+
+        public function getOggetto(){
+            return $this->oggetto;
+        }
+
+        public function setContenuto($contenuto){
+            $this->contenuto = $contenuto;
+        }
+
+        public function getContenuto(){
+            return $this->contenuto;
+        }
+        //fine getter e setter
+
+        public function invio(){
+            return 'Comunicazione inviata';
+        }
     }
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+    //usiamo il require_once per evitare eventuali errori
+    require_once __DIR__.'/Models/SistemiComunicazione.php';
+
+    $comm = new SistemiComunicazione('Fabrizio', 'Antonello', 'Saluto', 'Ciao Antonello');
+
+    //var_dump($comm);
+
+    //stampiamo a schermo
+    echo "Mittente: ".$comm->getMittente()."<br>";
+    echo "Destinatario: ".$comm->getDestinatario()."<br/>";
+    echo "Oggetto: ".$comm->getOggetto()."<br/>";
+    echo "Contenuto: ".$comm->getContenuto()."<br/>";
+    echo "Stato invio: ".$comm->invio()."<br/>";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
